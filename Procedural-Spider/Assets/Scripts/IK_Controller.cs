02cbdc073d72e0b5bef758b491784b13b42096aa
Ozan Ürkmez <arkantos250@gmail.com>
2020-11-29 15:39:32 +0300

    rotation fixing

--- Procedural-Spider/Assets/Scripts/IK_Controller.cs
+++ Procedural-Spider/Assets/Scripts/IK_Controller.cs
@@ -18,10 +18,17 @@ public class IK_Controller : MonoBehaviour
     [SerializeField]
     private float stopDelta;
 
+    //bone positioning
     protected float[] _bonesLength;
     protected float _completeLength;
     protected Transform[] _bones;
     protected Vector3[] _positions;
+
+    //bone rotations
+    protected Vector3[] _startDirSuccessive;
+    protected Quaternion[] _startRotationBone;
+    protected Quaternion _startRotationTarget;
+    protected Quaternion _startRotationRoot;
 
 
     private void Awake()
@@ -37,6 +44,10 @@ public class IK_Controller : MonoBehaviour
         _positions = new Vector3[_chainLength + 1];
         _bonesLength = new float[_chainLength];
 
+        _startDirSuccessive = new Vector3[_chainLength + 1];
+        _startRotationBone = new Quaternion[_chainLength + 1];
+        _startRotationTarget = _target.rotation;
+
         _completeLength = 0;
 
         //the lowest in the hierarchy is this one.
@@ -45,21 +56,29 @@ public class IK_Controller : MonoBehaviour
         for(int i = _bones.Length - 1; i >= 0; i--)
         {
             _bones[i] = current;
+            _startRotationBone[i] = current.rotation;
 
             if(i == _bones.Length - 1)
             {
                 //bone lowest in hierarchy. This has no length.
 
+                //this bone is called "leaf" formally
+
+                _startDirSuccessive[i] = _target.position - current.position;
             }
             else
             {
                 //all other bones
                 _bonesLength[i] = (_bones[i + 1].position - current.position).magnitude; //distance between us and next lower in hierarchy. 
                 _completeLength += _bonesLength[i];
+
+                _startDirSuccessive[i] = _bones[i + 1].position - current.position;
             }
 
             current = current.parent;
         }
+
+        _startRotationRoot = (_bones[0].parent != null) ? _bones[0].parent.rotation : Quaternion.identity;
     }
 
     private void LateUpdate()
@@ -81,6 +100,9 @@ public class IK_Controller : MonoBehaviour
         {
             _positions[i] = _bones[i].position;
         }
+
+        var rootRot = (_bones[0].parent != null) ? _bones[0].parent.rotation : Quaternion.identity;
+        var rootRotDiff = rootRot * Quaternion.Inverse(_startRotationRoot);
 
         //fun part!
 
@@ -134,6 +156,7 @@ public class IK_Controller : MonoBehaviour
         //set positions
         for (int i = 0; i < _bones.Length; i++)
         {
+            CorrectBoneRotation(i);
             _bones[i].position = _positions[i];  
         }
     }
@@ -163,6 +186,19 @@ public class IK_Controller : MonoBehaviour
 
         var angle = Vector3.SignedAngle(projectedBone - planeCenter, projectedPole - planeCenter, plane.normal);
         _positions[positionIndex] = Quaternion.AngleAxis(angle, plane.normal) * (_positions[positionIndex] - planeCenter) + planeCenter;
+    }
+
+    private void CorrectBoneRotation(int positionIndex)
+    {
+        if (positionIndex == _positions.Length - 1)
+        {
+            _bones[positionIndex].rotation = _target.rotation * Quaternion.Inverse(_startRotationTarget) * _startRotationBone[positionIndex];
+        }
+        else
+        {
+            _bones[positionIndex].rotation =
+                Quaternion.FromToRotation(_startDirSuccessive[positionIndex], _positions[positionIndex + 1] - _positions[positionIndex]) * _startRotationBone[positionIndex];
+        }
     }
 
     #endregion
